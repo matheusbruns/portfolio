3609aae11131af0579fd03c49f4c7b5d2408a48b
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-27 21:42:34 -0300

    texto animado

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ $(document).ready(function(){
 function onClickMenu(){
     document.querySelector('div.menu-btn').classList.toggle('change')
 }
+
+// animação do script
+var typed = new Typed(".typing", {
+    strings: ["Desenvolvedor", "Front-end"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
